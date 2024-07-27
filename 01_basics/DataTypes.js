6e1e02bf2501e760da29f0398b4c0624e36d1014
Musavvir Ahmed Khan <157981694+MusavvirKhan7@users.noreply.github.com>
2024-07-27 21:43:54 +0000

    datatypes

---
+++ 01_basics/DataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // this code treats JS code as newer version
+
+// alert(4+6) we are using Node, not browser  //
+
+// code readiblity should be a priority //
+
+console.log(3+5) 
+
+console.log("Musavvir")
+
+let name = "John" // String //
+let number = 1 // int //
+let isLoggedIn = "False" // boolean //
+let state = null // null //
+
+// number 2=> power of 52 
+// String => "text"
+// boolean => True/False
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof null); //object
+
+console.log(typeof undefined); //undefined
