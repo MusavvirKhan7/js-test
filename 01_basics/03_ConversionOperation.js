d9a855e39c9426ce7480e974f7f6651c59ae3f07
Musavvir Ahmed Khan <157981694+MusavvirKhan7@users.noreply.github.com>
2024-07-29 07:26:08 +0000

    ConversionOfDatatypes

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,28 @@
+let score = "32xyz"
+//"32" = 32
+//"32xyz" = NaN
+//undefined is NaN
+//null is 0
+//if TRUE value = 1 / if FALSE value = 0
+//
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInNumber =  Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 = TRUE; 0 = FALSE
+// "" = FALSE; "TEXT" = TRUE
+
+let somenumber = 33
+
+let StringNumber = String(somenumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
